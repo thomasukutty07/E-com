be18375c5b7792b84b073fb28eba0009a6250385
THOMASUKUTTY REJI <thomasukuttyreji7@gmail.com>
2025-07-12 18:34:33 +0530

    bug fixed 5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,13 +9,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://e-com-pc5u.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
+  build: {
+    rollupOptions: {
+      input: '/index.html', 
     },
+    outDir: 'dist',         
+    emptyOutDir: true      
   },
 })
