74d42032f24f4d98b90e4a49d6a69b87dad91f4f
THOMASUKUTTY REJI <thomasukuttyreji7@gmail.com>
2025-07-12 17:30:14 +0530

    Build React and set up backend static serving

--- backend/server.js
+++ backend/server.js
@@ -51,10 +51,10 @@ app.use("/api/shop/order", shopOrderRouter);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
 });
 
 // Start server
